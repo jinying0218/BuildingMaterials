eb8457289af2d93a965cb56bbb8556668445b242
Aries <jinying0218@163.com>
2014-10-24 02:14:09 +0800

    搭建主页面

---
+++ BuildingMaterials/APP/Controller/Main/TSCategoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  TSCategoryViewController.h
+//  BuildingMaterials
+//
+//  Created by Ariel on 14/10/24.
+//  Copyright (c) 2014年 Ariel. All rights reserved.
+//
+
+#import "TSBaseViewController.h"
+
+@interface TSCategoryViewController : TSBaseViewController
+
+@end
---
+++ BuildingMaterials/APP/Controller/Main/TSFristViewController.h
@@ -0,0 +1,13 @@
+//
+//  TSFristViewController.h
+//  BuildingMaterials
+//
+//  Created by Ariel on 14/10/24.
+//  Copyright (c) 2014年 Ariel. All rights reserved.
+//
+
+#import "TSBaseViewController.h"
+
+@interface TSFristViewController : TSBaseViewController
+
+@end
