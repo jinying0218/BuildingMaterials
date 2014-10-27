8b2cd12fc68154818ab20ff23a03d6653d082e3d
Aries <jinying0218@163.com>
2014-10-27 23:15:29 +0800

    首页自定义掌上秒杀cell

---
+++ BuildingMaterials/APP/View/TSGoodsRecommendTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TSGoodsRecommendTableViewCell.h
+//  BuildingMaterials
+//
+//  Created by Ariel on 14/10/27.
+//  Copyright (c) 2014年 Ariel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSGoodsRecommendTableViewCell : UITableViewCell
+
+@end
---
+++ BuildingMaterials/APP/View/TSSecondsDealTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TSSecondsDealTableViewCell.m
+//  BuildingMaterials
+//
+//  Created by Ariel on 14/10/27.
+//  Copyright (c) 2014年 Ariel. All rights reserved.
+//
+
+#import "TSSecondsDealTableViewCell.h"
+
+@implementation TSSecondsDealTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
