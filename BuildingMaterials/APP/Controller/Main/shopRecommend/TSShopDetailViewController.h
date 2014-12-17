b818eb00bae211d750e396a50fa342d605bd16ec
Aries <jinying0218@163.com>
2014-12-17 18:49:49 +0800

    商家详情

---
+++ BuildingMaterials/APP/Controller/Main/shopRecommend/TSShopDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  TSShopDetailViewController.h
+//  BuildingMaterials
+//
+//  Created by Ariel on 14/12/17.
+//  Copyright (c) 2014年 Ariel. All rights reserved.
+//
+
+#import "TSBaseViewController.h"
+
+@interface TSShopDetailViewController : TSBaseViewController
+
+@end
---
+++ BuildingMaterials/APP/View/TSShopDetailCollectionViewCell.m
@@ -0,0 +1,17 @@
+//
+//  TSShopDetailCollectionViewCell.m
+//  BuildingMaterials
+//
+//  Created by Ariel on 14/12/17.
+//  Copyright (c) 2014年 Ariel. All rights reserved.
+//
+
+#import "TSShopDetailCollectionViewCell.h"
+
+@implementation TSShopDetailCollectionViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
