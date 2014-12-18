4125a885f881c584deeddfd196429394a176f585
Aries <jinying0218@163.com>
2014-12-18 12:46:44 +0800

    商品详情页面基础UI

--- BuildingMaterials/APP/Controller/Main/shopRecommend/TSShopDetailViewController.m
+++ BuildingMaterials/APP/Controller/Main/shopRecommend/TSShopDetailViewController.m
@@ -10,6 +10,8 @@
 #import "TSShopDetailViewModel.h"
 #import "TSShopDetailCollectionViewCell.h"
 
+static  NSString *const ShopDetailCollectionHeaderIdentifier = @"ShopDetailCollectionHeaderIdentifier";
+
 @interface TSShopDetailViewController ()<UICollectionViewDataSource,UICollectionViewDelegate>
 @property (nonatomic, strong) UICollectionView *collectionView;
 @property (nonatomic, strong) NSMutableArray *collectViewDataArray;
@@ -61,6 +63,8 @@
     self.collectionView.dataSource = self;
     [self.rootView addSubview:self.collectionView];
 
+    [self.collectionView registerClass:[UICollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:ShopDetailCollectionHeaderIdentifier];
+
     /*
     CellConfigureBlock configureBlock = ^(TSShopRecommedDetailTableViewCell *cell,id taskModel,NSIndexPath *indexPath){
         [cell configureCellWithModel:taskModel indexPath:indexPath];
@@ -78,6 +82,70 @@
 {
     return 5;
 }
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
+    return 1;
+}
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section
+{
+    return CGSizeMake( KscreenW, 180);
+}
+
+//段头
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionReusableView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:ShopDetailCollectionHeaderIdentifier forIndexPath:indexPath];
+    for (UIView *subView in headerView.subviews) {
+        [subView removeFromSuperview];
+    }
+    headerView.backgroundColor = [UIColor whiteColor];
+    
+    UIScrollView *bannerScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake( 0, 0, KscreenW, 120)];
+    //    banner.showsVerticalScrollIndicator = NO;
+    //    banner.showsHorizontalScrollIndicator = NO;
+    bannerScrollView.contentSize = CGSizeMake( 3 * KscreenW, 120);
+    bannerScrollView.delegate = self;
+    bannerScrollView.pagingEnabled = YES;
+    bannerScrollView.backgroundColor = [UIColor yellowColor];
+    [headerView addSubview:bannerScrollView];
+    
+    UIImageView *imageView1 = [[UIImageView alloc] initWithImage:[UIImage imageNamedString:@"banner1"]];
+    imageView1.frame = CGRectMake( 0, 0, KscreenW, 120);
+    [bannerScrollView addSubview:imageView1];
+    
+    UIImageView *shopSymbolback = [[UIImageView alloc] initWithFrame:CGRectMake( 20, headerView.frame.size.height - 74, 70, 70)];
+    shopSymbolback.image = [UIImage imageNamedString:@"ss_06"];
+    [headerView addSubview:shopSymbolback];
+
+    UIImageView *shopSymbol = [[UIImageView alloc] initWithFrame:CGRectMake( 5, 5, 60, 60)];
+    shopSymbol.image = [UIImage imageNamedString:@"ss_09"];
+    [shopSymbolback addSubview:shopSymbol];
+    
+    UILabel *shopName = [[UILabel alloc] initWithFrame:CGRectMake( CGRectGetMaxX(shopSymbolback.frame) + 10, CGRectGetMaxY(bannerScrollView.frame) + 10, 100, 25)];
+    shopName.text = @"大自然地板";
+    shopName.textColor = [UIColor colorWithHexString:@"226e8a"];
+    shopName.font = [UIFont systemFontOfSize:17];
+//    shopName.backgroundColor = [UIColor redColor];
+    [headerView addSubview:shopName];
+    
+    UILabel *goodsKinds = [[UILabel alloc] initWithFrame:CGRectMake( CGRectGetMaxX(shopSymbolback.frame) + 10, CGRectGetMaxY(shopName.frame), 100, 20)];
+    goodsKinds.text = @"355种商品";
+//    goodsKinds.textColor = [UIColor colorWithHexString:@"226e8a"];
+    goodsKinds.font = [UIFont systemFontOfSize:14];
+//    goodsKinds.backgroundColor = [UIColor blueColor];
+    [headerView addSubview:goodsKinds];
+
+    UIButton *checkGoodsBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [checkGoodsBtn setBackgroundImage:[UIImage imageNamedString:@"shop_button_bg"] forState:UIControlStateNormal];
+    [checkGoodsBtn setTitle:@"查看全部商品" forState:UIControlStateNormal];
+    checkGoodsBtn.titleLabel.font = [UIFont systemFontOfSize:13];
+    checkGoodsBtn.frame = CGRectMake( KscreenW - 20 - 80, CGRectGetMaxY(bannerScrollView.frame) + 10, 80, 35);
+    [checkGoodsBtn bk_addEventHandler:^(id sender) {
+        NSLog(@"查看全部商品");
+    } forControlEvents:UIControlEventTouchUpInside];
+    [headerView addSubview:checkGoodsBtn];
+    return headerView;
+}
+
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     TSShopDetailCollectionViewCell *cell = [TSShopDetailCollectionViewCell cellWithCollectionView:collectionView indexPath:indexPath];
