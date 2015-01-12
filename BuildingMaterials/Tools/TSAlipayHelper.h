cc8767e50f1e8676c79d7da23a69e26548ba13bc
Aries <jinying0218@163.com>
2015-01-13 02:14:09 +0800

    添加支付宝SDK

---
+++ BuildingMaterials/Tools/TSAlipayHelper.h
@@ -0,0 +1,13 @@
+//
+//  TSAliypayHelper.h
+//  BuildingMaterials
+//
+//  Created by Ariel on 15/1/13.
+//  Copyright (c) 2015年 Ariel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TSAliypayHelper : NSObject
+
+@end
---
+++ BuildingMaterials/Tools/TSAlipayHelper.m
@@ -0,0 +1,13 @@
+//
+//  TSAliypayHelper.m
+//  BuildingMaterials
+//
+//  Created by Ariel on 15/1/13.
+//  Copyright (c) 2015年 Ariel. All rights reserved.
+//
+
+#import "TSAliypayHelper.h"
+
+@implementation TSAliypayHelper
+
+@end
