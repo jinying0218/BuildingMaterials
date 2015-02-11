3a51ef8bebb79853a88af3017281d58f0f4f9f2b
Aries <jinying0218@163.com>
2015-02-11 20:19:37 +0800

    no message

--- BuildingMaterials/APP/View/TSShopRecommedDetailTableViewCell.m
+++ BuildingMaterials/APP/View/TSShopRecommedDetailTableViewCell.m
@@ -26,11 +26,12 @@
         [self.shopImage sd_setImageWithURL:[NSURL URLWithString:model.COMPANY_IMAGE_URL] placeholderImage:[UIImage imageNamed:@"not_load"]];
     }
     self.shopName.text = model.COMPANY_NAME;
-    CGFloat descWidth = [self.shopName.text boundingRectWithSize:
-                          CGSizeMake(CGFLOAT_MAX, self.shopName.frame.size.height)
-                                                          options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading
-                                                       attributes:[NSDictionary dictionaryWithObjectsAndKeys:self.shopName.font,NSFontAttributeName, nil] context:nil].size.width;
-    self.shopName.frame = CGRectMake( 113, 5, descWidth, 21);
+    
+//    CGFloat descWidth = [self.shopName.text boundingRectWithSize:
+//                          CGSizeMake(CGFLOAT_MAX, self.shopName.frame.size.height)
+//                                                          options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading
+//                                                       attributes:[NSDictionary dictionaryWithObjectsAndKeys:self.shopName.font,NSFontAttributeName, nil] context:nil].size.width;
+//    self.shopName.frame = CGRectMake( 113, 5, descWidth, 21);
     
     self.shopDes.text = model.COMPANY_DES;
 //    if (model.IS_RECOMMEND) {
